721cc0f0519a96824a5456496b905e3c7820f218
Denis_Smok <den1989@inbox.ru>
2022-03-28 23:19:12 +0300

    multiply method

--- app/src/main/java/by/it/academy/App.java
+++ app/src/main/java/by/it/academy/App.java
@@ -10,5 +10,16 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        Calculator calculator=new Calculator();
+        System.out.println(calculator.multiply(3,4));
+    }
+
+    public class Calculator{
+        private int a;
+        private int b;
+
+      public int multiply(int a,int b){
+          return a*b;
+        }
     }
 }
